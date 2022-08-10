0017434c0fed5cbe4ffe6ea62509f8cf026dfa8a
TarekDjeddi <97988554+TarekDjeddi@users.noreply.github.com>
2022-08-10 11:03:19 -0400

    Delete python_exercises directory

--- python_exercises/python_pandas_app/test_csv_2_json.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from python_pandas_app.app_convert_csv_to_json import csv_2_json
-
-def test_csv_2_json():
-    csv_2_json('tests/temp.csv','tests/test_csv_2_json')
-    temp_json=pd.read_json('tests/temp.json')
-    test_json=pd.read_json('tests/test_csv_2_json.json')
-    assert(temp_json.equals(test_json))
